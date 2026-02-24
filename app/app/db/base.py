6c01b76642543fd3d1cc7a2c27b0f25478a7c622
Avinash Birudugadda <avinashbirudugadda@gmail.com>
2026-02-24 22:37:36 +0530

    Revert "Merge feature/core-backend into main (allow unrelated histories, prefer theirs)"
    This reverts commit 46991895b70ae8bb5250d8132d5ade848a46b163, reversing
    changes made to fdf348044da34e8383e4d23eda869bf79c731a2f.

--- app/app/db/base.py
+++
@@ -1,9 +0,0 @@
-from app.core.database import Base
-
-# Import ALL models here so SQLAlchemy registers them
-
-from app.models.country import Country
-from app.models.state import State
-from app.models.city import City
-from app.models.place import Place
-from app.models.place_media import PlaceMedia
